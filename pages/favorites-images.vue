52a26105186a6a731ab7744243ce6efe0c4e7573
Nurhanfront <front@studionomad.kz>
2021-08-12 13:03:47 +0600

    add: styles-favorites page

--- pages/favorites-images.vue
+++ pages/favorites-images.vue
@@ -1,24 +1,35 @@
 <template>
   <div class="p_page">
-    <toggle-columns />
-    <images :images="favorites"/>
+    <div class="title">
+      <h2>Избранное</h2>
+    </div>
+    <div class="favorites_images">
+      <toggle-columns />
+      <images :images="favorites" />
+    </div>
   </div>
 </template>
 
 <script>
 import Images from "../components/Images.vue";
-import Loader from '../components/loader.vue';
+import Loader from "../components/loader.vue";
 export default {
   components: { Images, Loader },
 
-  data:()=> ({
-    favorites: null,
+  layout: "innerPageHeader",
+
+  data: () => ({
+    favorites: null
   }),
 
   mounted() {
-      this.favorites = JSON.parse(localStorage.getItem('favorites'))
+    this.favorites = JSON.parse(localStorage.getItem("favorites"));
   }
 };
 </script>
 
-<style></style>
+<style lang="scss">
+  .favorites_images{
+    padding-top: 70px;
+  }
+</style>
